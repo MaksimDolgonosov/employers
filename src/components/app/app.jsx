ec0447cbc5d8738bc9e254ee86671c50f40718b9
Malsim Dauhanosav <max_air@bk.ru>
2023-03-07 20:33:48 +0300

    Компонент App сделал классовым

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,3 +1,5 @@
+import { Component } from "react";
+
 import "./app.css"
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
@@ -6,27 +8,43 @@ import AppFilter from "../app-filter/app-filter";
 import EmployersList from "../employers-list/employers-list";
 import EmployersAddForm from "../employers-add-form/employers-add-form";
 
-const App = () => {
-    const data = [
-        { name: "John C.", salary: 1800, increase: true, id: 1 },
-        { name: "Mike M.", salary: 3000, increase: false, id: 2 },
-        { name: "Alex S.", salary: 5000, increase: true, id: 3 },
-    ]
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: [
+                { name: "John C.", salary: 1800, increase: true, id: 1 },
+                { name: "Mike M.", salary: 3000, increase: false, id: 2 },
+                { name: "Alex S.", salary: 5000, increase: true, id: 3 },
+            ]
+        }
+    }
 
+    deleteItem=(id)=>{
+        console.log(id)
+        // this.setState(({data})=>{
+        //     console.log(data.id)
+        // })
+    }
 
-    return (
-        <div className="App">
-            <AppInfo />
-            <div className="search-panel">
-                <SearchPanel />
-                <AppFilter />
+    render() {
+        const { data } = this.state;
+        return (
+            <div className="App">
+                <AppInfo />
+                <div className="search-panel">
+                    <SearchPanel />
+                    <AppFilter />
+                </div>
+                <EmployersList
+                    data={data}
+                    onDelete={this.deleteItem} />
+                <EmployersAddForm />
             </div>
-            <EmployersList 
-            data={data}
-            onDelete={id=>console.log(id)} />
-            <EmployersAddForm />
-        </div>
-    )
+        )
+    }
+
+
 }
 
 export default App;
